4141e34459b8e1ab07a9856dc33e335ccdd96c3a
Matthew Hale <mjhale@valdosta.edu>
2020-02-10 18:31:27 +0000

    Modified pyTest.py

--- pyTest.py
+++ pyTest.py
@@ -1,2 +1,4 @@
 print("Printing some stuff out")
-Print("This is James Faber's branch")
+print("This was James Faber's branch")
+print("This is now Matt Hale's branch")
+print("Jk, this is actually the master branch")
